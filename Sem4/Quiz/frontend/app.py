b51a0e806909f700917e21656c83d9e3c077a056
Lukas Schreiber <lukas.schreiber@iu-study.org>
2024-06-21 12:26:02 +0200

    kivy struktur erstellt

---
+++ Sem4/Quiz/frontend/app.py
@@ -0,0 +1,28 @@
+# Importiert die Hauptklasse der Kivy-App
+from kivy.app import App
+
+# Importiert String- und Objekt-Eigenschaften aus Kivy
+from kivy.properties import StringProperty, ObjectProperty
+
+# Importiert verschiedene Layouts und Widgets aus Kivy
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.animation import Animation
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.uix.scrollview import ScrollView
+
+
+class MainWindow(Screen):
+    pass
+
+class FragenApp(App):
+    def build(self):
+        kv = Builder.load_file("my.kv")
+        self.icon = "sb.jpg"
+        return kv
+
+if __name__ == "__main__":
+    FragenApp().run()
